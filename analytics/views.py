60e8bdc107d3f58a5ff0ca631bb9f3c4a08e3d18
Abednego7 <isaacestampida7@gmail.com>
2024-11-28 19:10:34 -0300

    Description Analysis Section

--- analytics/views.py
+++ analytics/views.py
@@ -33,6 +33,49 @@ class HomeView(View):
             columns = df.columns.tolist()
             values = df.values.tolist()
             counts = df.count().tolist()
+
+            # Describe
+            general_statistics = df.describe().to_dict()
+
+            count = []
+            mean = []
+            std = []
+            min = []
+            percent_25 = []
+            percent_50 = []
+            percent_75 = []
+            max = []
+
+            for x in general_statistics.values():
+                for k, v in x.items():
+                    if k == "count":
+                        count.append(v)
+                    elif k == "mean":
+                        mean.append(v)
+                    elif k == "std":
+                        std.append(v)
+                    elif k == "min":
+                        min.append(v)
+                    elif k == "25%":
+                        percent_25.append(v)
+                    elif k == "50%":
+                        percent_50.append(v)
+                    elif k == "75%":
+                        percent_75.append(v)
+                    elif k == "max":
+                        max.append(v)
+
+            describe_info = {
+                "count": count,
+                "mean": mean,
+                "std": std,
+                "min": min,
+                "25%": percent_25,
+                "50%": percent_50,
+                "75%": percent_75,
+                "max": max,
+            }
+
             data_frame_info = {key: count for key, count in zip(columns, counts)}
 
             data_frame_context = {
@@ -45,7 +88,13 @@ class HomeView(View):
             return render(
                 request,
                 "analytics/home.html",
-                {"form": form, "name": name, "dataframe": data_frame_context},
+                {
+                    "form": form,
+                    "name": name,
+                    "dataframe": data_frame_context,
+                    "general_statistics": general_statistics.keys(),
+                    "describe_info": describe_info,
+                },
             )
 
         # To reload invalid form
