1e45211620c0146c7cb2ddcf8276510f623c964f
Abednego7 <isaacestampida7@gmail.com>
2025-01-25 12:59:22 -0300

    Paginator correction and adoption of SESSIONS to save information

--- analytics/views.py
+++ analytics/views.py
@@ -11,6 +11,9 @@ from io import BytesIO
 # Pandas
 import pandas as pd
 
+# To work with files directly with json
+from io import StringIO
+
 # Seaborn
 import matplotlib
 
@@ -23,8 +26,108 @@ import matplotlib.pyplot as plt
 # Create your views here.
 class HomeView(View):
     def get(self, request):
-        form = UploadFileForm()
-        return render(request, "analytics/home.html", {"form": form})
+        name = request.session.get("name")
+        # We retrieve the dataframe key stored in SESSIONS
+        df_json = request.session.get("dataframe")
+
+        if df_json:
+            df = pd.read_json(StringIO(df_json))
+
+            # Details
+            columns = df.columns.tolist()
+            values = df.values.tolist()
+            counts = df.count().tolist()
+            has_nulls = df.isnull().values.any()  # Bool
+
+            print(f"nulls ?: {has_nulls}")
+
+            # Pagination
+            paginator = Paginator(values, 10)
+            page_number = request.GET.get("page")
+            page_obj = paginator.get_page(page_number)
+
+            # Describe (General statistics)
+            general_statistics = df.describe().to_dict()
+
+            count = []
+            mean = []
+            std = []
+            min_des = []
+            percent_25 = []
+            percent_50 = []
+            percent_75 = []
+            max_des = []
+
+            for x in general_statistics.values():
+                for k, v in x.items():
+                    if k == "count":
+                        count.append(v)
+                    elif k == "mean":
+                        mean.append(v)
+                    elif k == "std":
+                        std.append(v)
+                    elif k == "min":
+                        min_des.append(v)
+                    elif k == "25%":
+                        percent_25.append(v)
+                    elif k == "50%":
+                        percent_50.append(v)
+                    elif k == "75%":
+                        percent_75.append(v)
+                    elif k == "max":
+                        max_des.append(v)
+
+            describe_info = {
+                "count": count,
+                "mean": mean,
+                "std": std,
+                "min": min_des,
+                "25%": percent_25,
+                "50%": percent_50,
+                "75%": percent_75,
+                "max": max_des,
+            }
+
+            data_frame_info = {key: count for key, count in zip(columns, counts)}
+
+            data_frame_context = {
+                "columns": columns,
+                "values": values,
+                "total_rows": data_frame_info,
+            }
+
+            # Graphs
+            plt.figure(figsize=(10, 6))
+            sns.heatmap(df.isnull(), cbar=False, cmap="viridis", yticklabels=False)
+            plt.title("Representation of Null values")
+
+            # Save the graph to memory as an image
+            buffer = BytesIO()
+            plt.savefig(buffer, format="png")
+            buffer.seek(0)
+            plt.close()
+
+            # Encode the image in base64
+            image_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+            buffer.close()
+
+            # "form": form -> To load valid form after submitting it
+            return render(
+                request,
+                "analytics/home.html",
+                {
+                    "form": UploadFileForm(),
+                    "name": name,
+                    "dataframe": data_frame_context,
+                    "page_obj": page_obj,
+                    "general_statistics": general_statistics.keys(),
+                    "describe_info": describe_info,
+                    "has_nulls": has_nulls,
+                    "chart_null": image_base64,
+                },
+            )
+        else:
+            return render(request, "analytics/home.html", {"form": UploadFileForm()})
 
     def post(self, request):
         form = UploadFileForm(request.POST, request.FILES)
@@ -61,6 +164,10 @@ class HomeView(View):
                             df[col] = df[col].fillna(df[col].mode()[0])
                 case _:
                     pass
+
+            # Save the dataframe in SESSIONS
+            request.session["name"] = name
+            request.session["dataframe"] = df.to_json()
 
             # Details
             columns = df.columns.tolist()
